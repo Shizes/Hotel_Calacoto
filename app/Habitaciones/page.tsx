d42ac19d698b04b50c5dd1dd62f49d16d7d341e8
Reyshel <reyshelortiz03@gmail.com>
2024-11-21 00:29:19 -0400

    primerintento

---
+++ app/Habitaciones/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { HeaderHabitaciones } from '@/components/HeaderHabitaciones';
+const Roomspage = () => {
+  return (
+    <div>
+        <HeaderHabitaciones />
+    </div>
+  )
+}
+
+export default Roomspage
