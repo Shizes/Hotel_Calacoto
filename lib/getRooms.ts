6aebe484548388a257d9e24481820c438f5911d1
KaitoEG <eaghtspr@gmail.com>
2024-11-21 09:39:11 -0400

    apis_R

--- lib/getRooms.ts
+++ lib/getRooms.ts
@@ -1,9 +1,11 @@
 export async function getRooms(url: string) {
     const response = await fetch(url.toString());
+    
     if (!response.ok) {
       console.error("Failed to fetch rooms, status:", response.status);
       throw new Error("Failed to fetch rooms");
     }
+  
     const data = await response.json();
     return data;
   }
