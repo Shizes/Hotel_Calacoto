086208e266beb0011a668ea07ceaf24dbb24ba90
Reyshel <reyshelortiz03@gmail.com>
2024-11-21 01:15:06 -0400

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
-
+import Navbar from "../components/ui/Navbar";
 export default function Home() {
   return (
-  <div></div>
+  <div>
+    <Navbar />
+      <main>
+        <h1>Bienvenido al Hotel Calacoto</h1>
+      </main>
+  </div>
   );
 }
