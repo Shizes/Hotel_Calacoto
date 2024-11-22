d6caafead31aebe24aac504a9c8500834eb28f56
Geraldiene <riomahabramromamateo@gmail.com>
2024-11-22 00:35:46 -0400

    generico

---
+++ app/armados/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../components/ui/Navbar'
+import Footer from '../../components/ui/Footer'
+
+const armados = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default armados
